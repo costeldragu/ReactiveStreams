578a58cd3f4682910ccb1de776cf72f128a7bc14
Dragu Costel <dragu.costel@yahoo.com>
2017-11-25 23:31:57 +0200

    Reactive Streams

---
+++ src/main/java/com/mdc/Main.java
@@ -0,0 +1,11 @@
+package com.mdc;
+
+/**
+ * @author : Costel DRAGU
+ * @version $Id:$
+ * @package : com.mdc
+ * @copyright Copyright (c) 2017
+ * @date : 25.11.2017 23:31
+ */
+public class Main {
+}
